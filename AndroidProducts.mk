598c68a30cae587d4697f44edeb2d883894bb657
hmdevs1002 <152835188+hmdevs1002@users.noreply.github.com>
2023-12-04 07:29:49 +0300

    Add AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_ares.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_ares-user \
+    lineage_ares-userdebug \
+    lineage_ares-eng
